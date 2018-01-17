9a1045930dc679b212234f2101bda394d3cbb3f9
Bilal Ahmed <bilalahmedim@gmail.com>
2018-01-17 13:50:43 +0530

    Every Thing Fine and Wrking

--- create.php
+++ create.php
@@ -1,22 +1,5 @@
 <?php
   include 'server.php';
-  if( isset($_POST['submit']) ) {
-    $ename = $_POST['name'];
-    $enum = $_POST['number'];
-    $esalary = $_POST['salary'];
-    if($enum==0 || $enum==""){
-      echo "<p>Enter Employee Number.</p>";
-    }if(strlen($ename) < 4){
-      echo "<p>Name must graterthan than 4.</p>";
-    }if($esalary==0 || $esalary < 1000){
-      echo "<p>Please Check Salary.</p>";
-    }
-    $create = "INSERT INTO records(id,name,basic_salary) VALUE('$enum','$ename','$esalary')";
-    $create_result=mysqli_query($connection,$create);
-    if(!$create_result){
-      die("Query Failed".mysqli_error());
-    }
-  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -44,9 +27,26 @@
       <input type="text" class="name" name="name" placeholder="Emloyee Name">
       <label for="basicsalary">Basic Salary</label>
       <input type="number" class="basicsalary" name="salary" placeholder="Basic Salary">
-      <input type="submit" value="submit" class="submit">
+      <input type="submit" name="submit" value="submit" class="submit">
       <div class="check">
       <?php
+      if( isset($_POST['submit']) ) {
+        $ename = $_POST['name'];
+        $enum = $_POST['number'];
+        $esalary = $_POST['salary'];
+        if($enum==0 || $enum==""){
+          echo "<p>Enter Employee Number.</p>";
+        }if(strlen($ename) < 4){
+          echo "<p>Name must graterthan than 4.</p>";
+        }if($esalary==0 || $esalary < 1000){
+          echo "<p>Please Check Salary.</p>";
+        }
+        $create = "INSERT INTO records(id,name,basic_salary) VALUE('$enum','$ename','$esalary')";
+        $create_result=mysqli_query($connection,$create);
+        if(!$create_result){
+          die("Query Failed".mysqli_error());
+        }
+      }
       ?>
     </div>
     </form>
