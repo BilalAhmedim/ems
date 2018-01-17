6d44dc252e8cb50569cac40cbfb26ac08251506a
Bilal Ahmed <bilalahmedim@gmail.com>
2018-01-17 12:18:19 +0530

    Add script and IgnoreFile and untrcked add in .gitignore

---
+++ script.js
@@ -0,0 +1,6 @@
+var x = document.querySelector(".check").childNodes.length;
+if(x == 1){
+    document.querySelector(".form").style.height="170px";
+}else{
+    document.querySelector(".check").style.opacity=0;
+}
