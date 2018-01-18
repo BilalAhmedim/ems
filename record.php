319234485ad61dfb892a4fb7f066cc0380f815f6
Bilal Ahmed <bilalahmedim@gmail.com>
2018-01-18 12:10:11 +0530

    Change records UI & form error message

--- record.php
+++ record.php
@@ -17,45 +17,19 @@
   <div class="datafeed">
     <table border=1>
       <tr>
-        <th>Day</th>
-        <th>No.</th>
-        <th>Name</th>
-        <th>Time In</th>
-        <th>Time Out</th>
-        <th>Total Time</th>
-        <th>Duty Period</th>
-        <th>Presence</th>
-        <th>Self Hollyday</th>
-        <th>Hollyday Cut</th>
-        <th>Over/Under Time</th>
-        <th>Per Hour</th>
-        <th>UO/OT</th>
+        <th>Employee No.</th>
+        <th>Employee Name</th>
         <th>Advance</th>
         <th>Expenses</th>
         <th>Basic Salary</th>
-        <th>Pay Salary Monthly</th>
-        <th>Pay By Day</th>
       </tr>
       <?php while($row=mysqli_fetch_array($read_result)) {?>
         <tr>
-          <td><div class="day"><?php echo $row['days'];?></div></td>
           <td><div class="no"><?php echo $row['id'];?></div></td>
           <td><div class="name"><?php echo $row['name'];?></div></td>
-          <td><div class="timein"><?php echo $row['time_in'];?></div></td>
-          <td><div class="timeout"><?php echo $row['time_out'];?></div></td>
-          <td><div class="totalt"><?php echo $row['total_time'];?></div></td>
-          <td><div class="duty_period"><?php echo $row['duty_period'];?></div></td>
-          <td><div class="presence"><?php echo $row['presence'];?></div></td>
-          <td><div class="self_hollyday"><?php echo $row['self_hollyday'];?></div></td>
-          <td><div class="hollyday_cut"><?php echo $row['hollyday_cut'];?></div></td>
-          <td><div class="total_underover_time"><?php echo $row['total_underover_time'];?></div></td>
-          <td><div class="per_hour"><?php echo $row['per_hour'];?></div></td>
-          <td><div class="undertime_overtime"><?php echo $row['ou_rs'];?></div></td>
           <td><div class="advance"><?php echo $row['advance'];?></div></td>
           <td><div class="expenses"><?php echo $row['expenses'];?></div></td>
           <td><div class="basic_salary"><?php echo $row['basic_salary'];?></div></td>
-          <td><div class="monthly_pay"><?php echo $row['pay_salary_month'];?></div></td>
-          <td><div class="pay_byday"><?php echo $row['pay_by_hour_day'];?></div></td>
         </tr>
       <?php } ?>
     </table>
