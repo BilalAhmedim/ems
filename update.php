09655a4349a75d89137c100d4f87f4c86a5eb19d
Bilal Ahmed <bilalahmedim@gmail.com>
2018-01-20 11:18:51 +0530

    Dynamicaly data read on DataBase in DailyUpdates ComboBox

--- update.php
+++ update.php
@@ -1,3 +1,6 @@
+<?php
+  include 'server.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,5 +19,19 @@
   <header>
     <h1>Daily Updates</h1>
   </header>
+  <div class="dailyupdates">
+    <form>
+      <label class="select" for="target">Select Employee : </label>
+      <select name="target">
+        <?php while($row=mysqli_fetch_array($summary_result)) {?>
+          <option value=<?php echo $row['id']; ?>>
+          <?php
+            echo $row['id']." ".$row['name'];
+          }
+        ?>
+        </option>
+      </select>
+    </form>
+  </div>
 </body>
 </html>
