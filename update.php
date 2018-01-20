f7c9e89e51104e457d25b487e5fafc0986b508bc
Bilal Ahmed <bilalahmedim@gmail.com>
2018-01-20 14:00:31 +0530

    Complite DailyUpdates Ui & Form Field not->php

--- update.php
+++ update.php
@@ -13,6 +13,14 @@
     body{
       background: linear-gradient(to right, #00f260, #0575e6);
     }
+    label{
+      font-weight: bold;
+    }
+    hr{
+      margin: 50px 0;
+      border-color: rgba(255,255,255,.5);
+      border-style: solid;
+    }
   </style>
 </head>
 <body>
@@ -21,16 +29,36 @@
   </header>
   <div class="dailyupdates">
     <form>
-      <label class="select" for="target">Select Employee : </label>
-      <select name="target">
-        <?php while($row=mysqli_fetch_array($summary_result)) {?>
+      <center>
+        <label class="select" for="target">Select Employee : </label>
+        <select name="target">
+        <option value="0">---Select an Option---</option>
+          <?php while($row=mysqli_fetch_array($summary_result)) {?>
           <option value=<?php echo $row['id']; ?>>
-          <?php
-            echo $row['id']." ".$row['name'];
-          }
-        ?>
-        </option>
-      </select>
+            <?php
+              echo $row['id']." ".$row['name'];
+            }
+            ?>
+          </option>
+        </select>
+      </center>
+    </form>
+    <hr>
+    <form method="post" class="updateform">
+			<label for="presence">Presence:</label>
+			<select name="presence">
+				<option value="1">Yes</option>
+				<option value="0">No</option>
+			</select>
+		  <label for="timein">Time In:</label>
+			<input type="time" name="timein">
+			<label for="timeout">Time Out:</label>
+			<input type="time" name="timeout">
+			<label for="advance">Advance:</label>
+			<input type="number" name="Advance">
+			<label for="expenses">Expenses:</label>
+			<input type="number" name="expenses"><br>
+			<input type="submit" name="submit">
     </form>
   </div>
 </body>
