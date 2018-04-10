1c4dc06aaf1ec7c0b4399d4ef056f5412b906ad2
Bilal Ahmed <bilalahmedim@gmail.com>
2018-04-10 13:04:29 +0530

    new branch with test code

--- update.php
+++ update.php
@@ -2,6 +2,157 @@
 include 'server.php';
 $id;
 $name;
+$con;
+$nulltimein = null;
+$nulltimeout = null;
+$id0 = null;
+$update1 = false;
+$update2 = false;
+$fn = false;
+function DataBase($query,$action){
+  $connect = mysqli_connect("localhost","root","","employee");
+  $con = $connect;
+  $sql = mysqli_query($connect, $query);
+  $result = mysqli_fetch_array($sql);
+  if(!$result){
+    $fn = true;
+    exit();
+  }
+  return $result[$action];
+}
+if (isset($_POST['submit'])) {
+  $presence = $_POST['presence'];
+  $timein = $_POST['timein'];
+  $timeout = $_POST['timeout'];
+  $advance = $_POST['advance'];
+  $expenses = $_POST['expenses'];
+  $id = $_POST['target'];
+  $total = (strtotime($timeout) - strtotime($timein)) - 3600;
+  $hours = floor($total / 3600);
+  $mins = floor($total / 60 % 60);
+  $secs = floor($total % 60);
+  $totaltime;
+  //  Read Info From Database
+  // get name
+  $name =  DataBase("SELECT name FROM summary WHERE id = $id",'name');
+  // get salary
+  $salary = DataBase("SELECT basic_salary FROM summary WHERE id = $id",'basic_salary');
+  // get day cut
+  $day_cut = $salary/30;
+  // per hour
+  $per_hour = $day_cut/8;
+  // get all advance expenses from summary
+  $total_advance = DataBase("SELECT advance FROM summary WHERE id = $id",'advance');
+  $total_expenses = DataBase("SELECT expenses FROM summary WHERE id = $id",'expenses');
+  // get time detuction
+  $td_query = DataBase("SELECT ou_rs FROM summary WHERE id = $id",'ou_rs');
+  // get hollyday cut from summary
+  $hollyday_cut = DataBase("SELECT hollyday_cut FROM summary WHERE id = $id" , 'hollyday_cut');
+  // get days
+  $days = DataBase("SELECT days FROM summary WHERE id = $id",'days');
+  $underover = $total-28800;
+  $total_underover_time = $underover / 60;
+  $ou_rs = ($total_underover_time / 60) * round($per_hour);
+  if ($id == 0) {
+    $id0 = "<p>Please Select Employee First.</p><br>";
+    exit();
+  }
+  if ($presence == 1) {
+    $totaltime = sprintf('%02d:%02d:%02d', $hours, $mins, $secs);
+    if ($timein == 0 or $timein == " ") {
+      $nulltimein = "<p>Enter Entry Time.</p><br>";
+      exit();
+    }
+    if ($timeout == 0 or $timeout == "") {
+      $nulltimeout = "<p>Enter Exit Time.</p><br>";
+      exit();
+    }
+    $duty_period = $totaltime;
+    $duty = (strtotime($timeout) - strtotime($timein));
+    $newmins = floor($duty / 60 % 60);
+    $newsecs = floor($duty % 60);
+    if($duty >= 53400){
+      $newhours = floor(($duty + 3600 ) / 3600);
+      $newmins = floor($duty / 60 % 60);
+      $newsecs = floor($duty % 60);
+      $time = $newhours.".".$newmins;
+      $duty_period = sprintf('%02d:%02d:%02d', $newhours, $newmins, $newsecs);
+      $total_underover_time = $total_underover_time + 121;
+      $ou_rs = ($total_underover_time / 60) * round($per_hour);
+    }else if($duty >= 39000 ){
+      $newhours = floor( $duty / 3600);
+      $newmins = floor($duty / 60 % 60);
+      $newsecs = floor($duty % 60);
+      $time = $newhours.".".$newmins;
+      $duty_period = sprintf('%02d:%02d:%02d', $newhours, $newmins, $newsecs);
+      $total_underover_time = $total_underover_time + 60;
+      $ou_rs = ($total_underover_time / 60) * round($per_hour);
+    }else{
+      $time = $hours.".".$mins;
+    }
+    $pay_by_hour_day = round($time * $per_hour);
+    //  Update data on Database
+    $create = "UPDATE summary SET total_underover_time = total_underover_time + '$total_underover_time', ou_rs = ou_rs + '$ou_rs' ,days = days + 1 , presence = '$presence' , advance =advance + '$advance' , expenses = expenses + '$expenses' WHERE id = $id";
+    $create_result = mysqli_query($connect, $create);
+    if (! $create_result) {
+      $update1 = true;
+      exit();
+    }
+      // get hollyday cut from summary
+    $hollyday_cut = DataBase("SELECT hollyday_cut FROM summary WHERE id = $id" , 'hollyday_cut');
+    // get time detuction
+    $td_query = DataBase("SELECT ou_rs FROM summary WHERE id = $id",'ou_rs');
+    // get all advance expenses from summary
+    $total_advance = DataBase("SELECT advance FROM summary WHERE id = $id",'advance');
+    $total_expenses = DataBase("SELECT expenses FROM summary WHERE id = $id",'expenses');
+    //  main equation for payable amount
+    $pay_salary_month = $salary -( $hollyday_cut + $total_advance + $total_expenses ) + $td_query;
+    $ontable = "INSERT INTO " . $name . "(pay_salary_month, pay_by_hour_day, basic_salary, ou_rs, days,total_underover_time, per_hour, id, name, duty_period, presence, time_in, time_out, total_time, advance, expenses) 
+    VALUE('$pay_salary_month','$pay_by_hour_day','$salary','$ou_rs','1','$total_underover_time','$per_hour','$id','$name','$duty_period','$presence','$timein','$timeout','$totaltime','$advance','$expenses')";
+    $insert = mysqli_query($connect, $ontable);
+    if (! $insert) {
+      $insert1 = true;
+      exit();
+    }
+    $create = "UPDATE summary SET pay_salary_month = '$pay_salary_month' WHERE id = $id";
+    $create_result = mysqli_query($connect, $create);
+    if (! $create_result) {
+      $update2 = true;
+      exit();
+    }
+  }
+  else {
+    // main Equation for payable amount
+    $create = "UPDATE summary SET self_hollyday = self_hollyday + '1', hollyday_cut = hollyday_cut + '$day_cut', presence = '$presence' WHERE id = $id";
+    $create_result = mysqli_query($connect, $create);
+    if (! $create_result) {
+      $update1 = true;
+      exit();
+    }
+    // get hollyday cut from summary
+    $hollyday_cut = DataBase("SELECT hollyday_cut FROM summary WHERE id = $id" , 'hollyday_cut');
+    // get time detuction
+    $td_query = DataBase("SELECT ou_rs FROM summary WHERE id = $id",'ou_rs');
+    // get all advance expenses from summary
+    $total_advance = DataBase("SELECT advance FROM summary WHERE id = $id",'advance');
+    $total_expenses = DataBase("SELECT expenses FROM summary WHERE id = $id",'expenses');
+    //  main equation for payable amount
+    $pay_salary_month = $salary -( $hollyday_cut + $total_advance + $total_expenses ) + $td_query;
+    $ontable = "INSERT INTO " . $name . "(pay_salary_month,id,name,presence,self_hollyday,hollyday_cut) VALUE('$pay_salary_month','$id','$name','$presence','1','$day_cut')";
+    $insert = mysqli_query($connect, $ontable);
+    if (! $insert) {
+      $insert1 = true;
+      exit();
+    }
+    $create = "UPDATE summary SET pay_salary_month = '$pay_salary_month' WHERE id = $id";
+    $create_result = mysqli_query($connect, $create);
+    if (! $create_result) {
+      $update2 = true;
+      exit();
+    }
+  }
+  //header("Location: http://localhost/update.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -42,12 +193,10 @@ hr {
         <label class="select" for="target">Select Employee : </label> <select name="target">
           <option value="0">---Select an Option---</option>
             <?php while($row=mysqli_fetch_array($summary_result)) {?>
-            <option value=<?php echo $row['id']; ?>>
             <?php
-              echo $row['id'] . " " . $row['name'];
+              echo '<option value='. $row['id'].'>'.$row['id'] . ' ' . $row['name'] . '</option>';
             }
             ?>
-            </option>
         </select>
       </center>
       <label for="presence">Presence: </label> <select name="presence">
@@ -57,143 +206,30 @@ hr {
         name="timein"> <label for="timeout">Time Out: </label> <input
         type="time" name="timeout"> <label for="advance">Advance: </label>
       <input type="number" name="advance"> <label for="expenses">Expenses:</label>
-      <input type="number" name="expenses"> <br> <input type="submit" name="submit"> <label class="check">
-        <?php
-        function DataBase($query,$action){
-          $connect = mysqli_connect("localhost","root","","employee");
-          $sql = mysqli_query($connect, $query);
-          $result = mysqli_fetch_array($sql);
-          if(!$result){
+      <input type="number" name="expenses"> <br> <input type="submit" name="submit"> <label class="check" name="massage">
+      <?php
+      if($fn == true){
+        die("Query Failed " . mysqli_error($con));
+      }
+        if(isset($_POST['submit'])){
+          if($update1 == true){
             die("Query Failed " . mysqli_error($connect));
           }
-          return $result[$action];
-        }
-        if (isset($_POST['submit'])) {
-          $presence = $_POST['presence'];
-          $timein = $_POST['timein'];
-          $timeout = $_POST['timeout'];
-          $advance = $_POST['advance'];
-          $expenses = $_POST['expenses'];
-          $id = $_POST['target'];
-          $total = (strtotime($timeout) - strtotime($timein)) - 3600;
-          $hours = floor($total / 3600);
-          $mins = floor($total / 60 % 60);
-          $secs = floor($total % 60);
-          $totaltime;
-          //  Read Info From Database
-          // get name
-          $name =  DataBase("SELECT name FROM summary WHERE id = $id",'name');
-          // get salary
-          $salary = DataBase("SELECT basic_salary FROM summary WHERE id = $id",'basic_salary');
-          // get day cut
-          $day_cut = $salary/30;
-          // per hour
-          $per_hour = $day_cut/8;
-          // get all advance expenses from summary
-          $total_advance = DataBase("SELECT advance FROM summary WHERE id = $id",'advance');
-          $total_expenses = DataBase("SELECT expenses FROM summary WHERE id = $id",'expenses');
-          // get time detuction
-          $td_query = DataBase("SELECT ou_rs FROM summary WHERE id = $id",'ou_rs');
-          // get hollyday cut from summary
-          $hollyday_cut = DataBase("SELECT hollyday_cut FROM summary WHERE id = $id" , 'hollyday_cut');
-          // get days
-          $days = DataBase("SELECT days FROM summary WHERE id = $id",'days');
-          $underover = $total-28800;
-          $total_underover_time = $underover / 60;
-          $ou_rs = ($total_underover_time / 60) * round($per_hour);
-          if ($id == 0) {
-            echo "<p>Please Select Employee First.</p><br>";
-            exit();
+          if($insert1 == true){
+            die("Query Failed " . mysqli_error($connect));
           }
-          if ($presence == 1) {
-            $totaltime = sprintf('%02d:%02d:%02d', $hours, $mins, $secs);
-            if ($timein == 0 or $timein == " ") {
-              echo "<p>Enter Entry Time.</p><br>";
-              exit();
-            }
-            if ($timeout == 0 or $timeout == "") {
-              echo "<p>Enter Exit Time.</p><br>";
-              exit();
-            }
-            $duty_period = $totaltime;
-            $duty = (strtotime($timeout) - strtotime($timein));
-            $newmins = floor($duty / 60 % 60);
-            $newsecs = floor($duty % 60);
-            if($duty >= 53400){
-              $newhours = floor(($duty + 3600 ) / 3600);
-              $newmins = floor($duty / 60 % 60);
-              $newsecs = floor($duty % 60);
-              $time = $newhours.".".$newmins;
-              $duty_period = sprintf('%02d:%02d:%02d', $newhours, $newmins, $newsecs);
-              $total_underover_time = $total_underover_time + 121;
-              $ou_rs = ($total_underover_time / 60) * round($per_hour);
-            }else if($duty >= 39000 ){
-              $newhours = floor( $duty / 3600);
-              $newmins = floor($duty / 60 % 60);
-              $newsecs = floor($duty % 60);
-              $time = $newhours.".".$newmins;
-              $duty_period = sprintf('%02d:%02d:%02d', $newhours, $newmins, $newsecs);
-              $total_underover_time = $total_underover_time + 60;
-              $ou_rs = ($total_underover_time / 60) * round($per_hour);
-            }else{
-              $time = $hours.".".$mins;
-            }
-            $pay_by_hour_day = round($time * $per_hour);
-            //  Update data on Database
-            $create = "UPDATE summary SET total_underover_time = total_underover_time + '$total_underover_time', ou_rs = ou_rs + '$ou_rs' ,days = days + 1 , presence = '$presence' , advance =advance + '$advance' , expenses = expenses + '$expenses' WHERE id = $id";
-            $create_result = mysqli_query($connect, $create);
-            if (! $create_result) {
-              die("Query Failed " . mysqli_error($connect));
-            }
-             // get hollyday cut from summary
-            $hollyday_cut = DataBase("SELECT hollyday_cut FROM summary WHERE id = $id" , 'hollyday_cut');
-            // get time detuction
-            $td_query = DataBase("SELECT ou_rs FROM summary WHERE id = $id",'ou_rs');
-            // get all advance expenses from summary
-            $total_advance = DataBase("SELECT advance FROM summary WHERE id = $id",'advance');
-            $total_expenses = DataBase("SELECT expenses FROM summary WHERE id = $id",'expenses');
-            //  main equation for payable amount
-            $pay_salary_month = $salary -( $hollyday_cut + $total_advance + $total_expenses ) + $td_query;
-            $ontable = "INSERT INTO " . $name . "(pay_salary_month, pay_by_hour_day, basic_salary, ou_rs, days,total_underover_time, per_hour, id, name, duty_period, presence, time_in, time_out, total_time, advance, expenses) 
-            VALUE('$pay_salary_month','$pay_by_hour_day','$salary','$ou_rs','1','$total_underover_time','$per_hour','$id','$name','$duty_period','$presence','$timein','$timeout','$totaltime','$advance','$expenses')";
-            $insert = mysqli_query($connect, $ontable);
-            if (! $insert) {
-              die("Query Failed " . mysqli_error($connect));
-            }
-            $create = "UPDATE summary SET pay_salary_month = '$pay_salary_month' WHERE id = $id";
-            $create_result = mysqli_query($connect, $create);
-            if (! $create_result) {
-              die("Query Failed " . mysqli_error($connect));
-            }
+          if($update2 == true){
+            die("Query Failed " . mysqli_error($connect));
           }
-          else {
-            // main Equation for payable amount
-            $create = "UPDATE summary SET self_hollyday = self_hollyday + '1', hollyday_cut = hollyday_cut + '$day_cut', presence = '$presence' WHERE id = $id";
-            $create_result = mysqli_query($connect, $create);
-            if (! $create_result) {
-              die("Query Failed " . mysqli_error($connect));
-            }
-            // get hollyday cut from summary
-            $hollyday_cut = DataBase("SELECT hollyday_cut FROM summary WHERE id = $id" , 'hollyday_cut');
-            // get time detuction
-            $td_query = DataBase("SELECT ou_rs FROM summary WHERE id = $id",'ou_rs');
-            // get all advance expenses from summary
-            $total_advance = DataBase("SELECT advance FROM summary WHERE id = $id",'advance');
-            $total_expenses = DataBase("SELECT expenses FROM summary WHERE id = $id",'expenses');
-            //  main equation for payable amount
-            $pay_salary_month = $salary -( $hollyday_cut + $total_advance + $total_expenses ) + $td_query;
-            $ontable = "INSERT INTO " . $name . "(pay_salary_month,id,name,presence,self_hollyday,hollyday_cut) VALUE('$pay_salary_month','$id','$name','$presence','1','$day_cut')";
-            $insert = mysqli_query($connect, $ontable);
-            if (! $insert) {
-              die("Query Failed " . mysqli_error($connect));
-            }
-            $create = "UPDATE summary SET pay_salary_month = '$pay_salary_month' WHERE id = $id";
-            $create_result = mysqli_query($connect, $create);
-            if (! $create_result) {
-              die("Query Failed " . mysqli_error($connect));
-            }
+          if($id0 != null){
+            echo $id0;
           }
-          header("Location: http://localhost/update.php");
+          if($nulltimein != null){
+            echo $nulltimein;
+          }
+          if($nulltimeout != null){
+            echo $nulltimeout;
+          }
         }
       ?>
       </label>
